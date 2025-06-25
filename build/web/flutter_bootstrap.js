af1319b5db5735a1d92bebb1803fd94eb21a2c9b
Takuma Ono <oono20030411@gmail.com>
2025-06-25 17:02:57 +0900

    修正

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3221280291"
+    serviceWorkerVersion: "2331437305"
   }
 });
